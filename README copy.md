cb34e9334280a9b19b5da1b362fd526ef4c6f523
Nathan Waxman-Jeng <54194229+nthnwj@users.noreply.github.com>
2025-10-14 19:26:36 -0700

    Add initial kit bot code

---
+++ README copy.md
@@ -0,0 +1,3 @@
+# 2025-Training
+
+This is the skeleton code for our kit bot, that we'll be using for our FRC software training.
---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,8 @@
+package frc.robot;
+
+public class Constants {
+    public static final int LEFT_DRIVETRAIN_MOTOR_ID = 1;
+    public static final int RIGHT_DRIVETRAIN_MOTOR_ID = 2;
+    public static final int ARM_MOTOR_ID = 3;
+    public static final int LIMIT_SWItCH_ID = 0;
+}
